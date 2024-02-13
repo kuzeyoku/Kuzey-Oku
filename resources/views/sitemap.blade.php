a2b7955e81b0f00b3e17092a705326a8350ef210
Kuzey Oku Software <171201010@ogrenci.karatekin.edu.tr>
2024-02-13 13:33:00 +0300

    sitemap

--- resources/views/sitemap.blade.php
+++ resources/views/sitemap.blade.php
@@ -1,6 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-
     <url>
         <loc>{{ url('/') }}/</loc>
         <changefreq>{{ config('setting.sitemap.home_changefreq') }}</changefreq>
@@ -11,6 +10,21 @@
         <changefreq>{{ config('setting.sitemap.service_changefreq') }}</changefreq>
         <priority>{{ config('setting.sitemap.service_priority') }}</priority>
     </url>
+    <url>
+        <loc>{{ url(route(\App\Enums\ModuleEnum::Project->route() . '.index')) }}/</loc>
+        <changefreq>{{ config('setting.sitemap.project_changefreq') }}</changefreq>
+        <priority>{{ config('setting.sitemap.project_priority') }}</priority>
+    </url>
+    <url>
+        <loc>{{ url(route(\App\Enums\ModuleEnum::Product->route() . '.index')) }}/</loc>
+        <changefreq>{{ config('setting.sitemap.product_changefreq') }}</changefreq>
+        <priority>{{ config('setting.sitemap.product_priority') }}</priority>
+    </url>
+    <url>
+        <loc>{{ url(route(\App\Enums\ModuleEnum::Blog->route() . '.index')) }}/</loc>
+        <changefreq>{{ config('setting.sitemap.blog_changefreq') }}</changefreq>
+        <priority>{{ config('setting.sitemap.blog_priority') }}</priority>
+    </url>
     @foreach ($services as $service)
         <url>
             <loc>{{ url($service->url) }}/</loc>
@@ -18,11 +32,6 @@
             <priority>{{ config('setting.sitemap.service_detail_priority') }}</priority>
         </url>
     @endforeach
-    <url>
-        <loc>{{ url(route(\App\Enums\ModuleEnum::Project->route() . '.index')) }}/</loc>
-        <changefreq>{{ config('setting.sitemap.project_changefreq') }}</changefreq>
-        <priority>{{ config('setting.sitemap.project_priority') }}</priority>
-    </url>
     @foreach ($projects as $project)
         <url>
             <loc>{{ url($project->url) }}/</loc>
@@ -30,11 +39,6 @@
             <priority>{{ config('setting.sitemap.project_detail_priority') }}</priority>
         </url>
     @endforeach
-    <url>
-        <loc>{{ url(route(\App\Enums\ModuleEnum::Product->route() . '.index')) }}/</loc>
-        <changefreq>{{ config('setting.sitemap.product_changefreq') }}</changefreq>
-        <priority>{{ config('setting.sitemap.product_priority') }}</priority>
-    </url>
     @foreach ($products as $product)
         <url>
             <loc>{{ url($product->url) }}/</loc>
@@ -42,28 +46,11 @@
             <priority>{{ config('setting.sitemap.product_detail_priority') }}</priority>
         </url>
     @endforeach
-    <url>
-        <loc>{{ url(route(\App\Enums\ModuleEnum::Blog->route() . '.index')) }}/</loc>
-        <changefreq>{{ config('setting.sitemap.blog_changefreq') }}</changefreq>
-        <priority>{{ config('setting.sitemap.blog_priority') }}</priority>
-    </url>
     @foreach ($posts as $post)
         <url>
             <loc>{{ url($post->url) }}/</loc>
             <changefreq>{{ config('setting.sitemap.blog_detail_changefreq') }}</changefreq>
             <priority>{{ config('setting.sitemap.blog_detail_priority') }}</priority>
-        </url>
-    @endforeach
-    <url>
-        <loc>{{ url(route(\App\Enums\ModuleEnum::Education->route() . '.index')) }}/</loc>
-        <changefreq>{{ config('setting.sitemap.education_changefreq') }}</changefreq>
-        <priority>{{ config('setting.sitemap.education_priority') }}</priority>
-    </url>
-    @foreach ($educations as $education)
-        <url>
-            <loc>{{ url($education->url) }}/</loc>
-            <changefreq>{{ config('setting.sitemap.education_detail_changefreq') }}</changefreq>
-            <priority>{{ config('setting.sitemap.education_detail_priority') }}</priority>
         </url>
     @endforeach
     @foreach ($pages as $page)
