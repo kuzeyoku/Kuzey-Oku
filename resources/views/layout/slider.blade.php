70f8a14b8d09bc6290fc63f2b2c69be0b97982d7
Kuzey Oku Software <171201010@ogrenci.karatekin.edu.tr>
2024-02-13 13:03:54 +0300

    slider

--- resources/views/layout/slider.blade.php
+++ resources/views/layout/slider.blade.php
@@ -32,7 +32,7 @@
                             data-textalign="['top','top','top','top']"
                             data-frames='[{"delay":1000,"speed":1500,"frame":"0","from":"y:[-100%];z:0;rX:0deg;rY:0;rZ:0;sX:1;sY:1;skX:0;skY:0;","mask":"x:0px;y:0px;s:inherit;e:inherit;","to":"o:1;","ease":"Power3.easeInOut"},{"delay":"wait","speed":300,"frame":"999","to":"auto:auto;","ease":"Power3.easeInOut"}]'>
                             <h1>{{ $slider->title }} </h1>
-                            <h3 class="style-font color3">{{ $slider->description }}</h3>
+                            <p class="style-font color3">{{ $slider->description }}</p>
                         </div>
                         <div class="tp-caption" data-paddingbottom="[0,0,0,0]" data-paddingleft="[15,15,15,15]"
                             data-paddingright="[15,15,15,15]" data-paddingtop="[0,0,0,0]" data-responsive_offset="on"
