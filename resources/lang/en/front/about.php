cecf4309c9ada4c0ae2964414c1cd04da3ce507a
Kuzey Oku Software <171201010@ogrenci.karatekin.edu.tr>
2024-05-18 13:14:48 +0300

    update

--- resources/lang/en/front/about.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    'txt1' => 'Biz Kimiz ?',
-    'txt2' => 'Kuzey Oku | Harita ve Madencilik',
-    'txt3' => 'There are many variations of passages of available but the majority have suffered. Alteration in some form, lipsum is simply free text by injected humou or randomised words even believable.',
-    'txt4' => 'Lorem ipsum dolor sit amet, consectetur notted adipisicing elit sed do eiusmod',
-    'txt5' => 'Tanıtım Videomuzu <br> İzlediniz mi ?',
-    'txt6' => 'Hakkımızda',
-];
--- resources/lang/en/front/general.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return [
-    "recaptcha_error" => "Robot Doğrulaması Başarısız",
-    "newsletter_success" => "Bülten Aboneliği Kaydınız Alındı",
-    "newsletter_error" => "İşlem sırasında bir hata oluştu",
-];
