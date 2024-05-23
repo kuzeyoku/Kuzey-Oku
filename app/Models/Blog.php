3d393fc6cdbb59636952c73ac1af4ef649e994dc
Kuzey Oku Software <171201010@ogrenci.karatekin.edu.tr>
2024-05-23 12:49:32 +0300

    e

--- app/Models/Blog.php
+++ app/Models/Blog.php
@@ -9,8 +9,7 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Spatie\MediaLibrary\HasMedia;
 use Spatie\MediaLibrary\InteractsWithMedia;
-use Spatie\Image\Manipulations;
-use Spatie\MediaLibrary\MediaCollections\Models\Media;
+
 
 class Blog extends Model implements HasMedia
 {
