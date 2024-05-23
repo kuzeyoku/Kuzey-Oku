d6867ae418e63be8ef614de8569628d30371abed
Kuzey Oku Software <171201010@ogrenci.karatekin.edu.tr>
2024-05-23 19:47:12 +0300

    update

--- config/filesystems.php
+++ config/filesystems.php
@@ -39,7 +39,7 @@ return [
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
-            'url' => env('APP_URL') . '/storage',
+            'url' => env('APP_URL') . '/public/storage',
             'visibility' => 'public',
             'throw' => false,
         ],
@@ -47,7 +47,7 @@ return [
         'media' => [
             'driver' => 'local',
             'root' => storage_path('app/public/media'),
-            'url' => env('APP_URL') . '/storage/media',
+            'url' => env('APP_URL') . '/public/storage/media',
             'visibility' => 'public',
             'throw' => false,
         ],
