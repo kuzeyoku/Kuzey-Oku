974228f97c2de4df648206ab3e5b20d66d984766
Kuzey Oku Software <171201010@ogrenci.karatekin.edu.tr>
2024-05-30 11:44:54 +0300

    media

--- app/Http/Controllers/Admin/MediaController.php
+++ app/Http/Controllers/Admin/MediaController.php
@@ -2,28 +2,25 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Enums\ModuleEnum;
 use App\Http\Controllers\Controller;
 use App\Services\Admin\MediaService;
 use Spatie\MediaLibrary\MediaCollections\Models\Media;
 
 class MediaController extends Controller
 {
-
-    protected $service;
-
-    public function __construct(MediaService $service)
+    public function __construct()
     {
-        $this->service = $service;
         view()->share([
-            "route" => $this->service->route(),
-            "folder" => $this->service->folder()
+            "route" => ModuleEnum::Media->route(),
+            "folder" => ModuleEnum::Media->folder(),
         ]);
     }
 
     public function index()
     {
         $items = Media::orderByDesc("id")->paginate(18);
-        return view(themeView("admin", "{$this->service->folder()}.index"), compact("items"));
+        return view(themeView("admin", "" . ModuleEnum::Media->folder() . ".index"), compact("items"));
     }
 
     public function destroy($media_id)
@@ -31,10 +28,9 @@ class MediaController extends Controller
         try {
             $media = Media::findOrFail($media_id);
             $media->delete();
-            return back()->withSuccess(__("admin/{$this->service->folder()}.delete_success"));
+            return back()->withSuccess(__("admin/" . ModuleEnum::Media->folder() . ".delete_success"));
         } catch (\Exception $e) {
-            dd($e->getMessage());
-            return back()->withErrors(__("admin/{$this->service->folder()}.delete_error"));
+            return back()->withErrors(__("admin/" . ModuleEnum::Media->folder() . ".delete_error"));
         }
     }
 }
