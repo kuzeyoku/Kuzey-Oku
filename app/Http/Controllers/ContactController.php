ce6039118a9e32013d9aae1a36e890af16b8d1ee
Kuzey Oku Software <171201010@ogrenci.karatekin.edu.tr>
2024-06-11 16:03:15 +0300

    sqs

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -23,7 +23,7 @@ class ContactController extends Controller
                 ->withError(__("front/general.recaptcha_error"));
         }
         try {
-           // $this->dispact(new SendMessage($request));
+            $this->dispact(new SendMessage($request));
             Message::create([
                 "name" => $request->name,
                 "phone" => $request->phone,
