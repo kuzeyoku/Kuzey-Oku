0de53024faca4ff3e166523ada162a72ca26d8a0
Kuzey Oku Software <171201010@ogrenci.karatekin.edu.tr>
2024-06-13 14:59:41 +0300

    a

--- app/Providers/MenuProvider.php
+++ app/Providers/MenuProvider.php
@@ -23,38 +23,38 @@ class MenuProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        $cache = cache();
-        $cacheTime = settings("caching.time", 3600);
+        // $cache = cache();
+        // $cacheTime = settings("caching.time", 3600);
 
-        view()->composer("layout.footer", function ($view) use ($cache, $cacheTime) {
+        // view()->composer("layout.footer", function ($view) use ($cache, $cacheTime) {
 
-            $informationPages = settings("information");
-            if ($informationPages) {
-                unset($informationPages["cookie_notification_status"]);
-                $pages = $cache->remember("footerInformationPages_" . app()->getLocale(), $cacheTime, function () use ($informationPages) {
-                    foreach ($informationPages as $key => $value) {
-                        if ($value)
-                            $page[$key] = Page::findOrFail($value);
-                    }
-                    return $page ?? [];
-                });
-            } else {
-                $pages = [];
-            }
+        //     $informationPages = settings("information");
+        //     if ($informationPages) {
+        //         unset($informationPages["cookie_notification_status"]);
+        //         $pages = $cache->remember("footerInformationPages_" . app()->getLocale(), $cacheTime, function () use ($informationPages) {
+        //             foreach ($informationPages as $key => $value) {
+        //                 if ($value)
+        //                     $page[$key] = Page::findOrFail($value);
+        //             }
+        //             return $page ?? [];
+        //         });
+        //     } else {
+        //         $pages = [];
+        //     }
 
-            $services = $cache->remember("footerServices_" . app()->getLocale(), $cacheTime, function () {
-                return Service::whereStatus(StatusEnum::Active->value)->limit(6)->get();
-            });
+        //     $services = $cache->remember("footerServices_" . app()->getLocale(), $cacheTime, function () {
+        //         return Service::whereStatus(StatusEnum::Active->value)->limit(6)->get();
+        //     });
 
-            $view->with(compact("pages", "services"));
-        });
+        //     $view->with(compact("pages", "services"));
+        // });
 
-        view()->composer("layout.header", function ($view) use ($cache, $cacheTime) {
-            $headerMenu = $cache->remember("headerMenu_" . app()->getLocale(), $cacheTime, function () {
-                return Menu::whereType("header")->order()->get();
-            });
-            $view->with(compact("headerMenu"));
-        });
+        // view()->composer("layout.header", function ($view) use ($cache, $cacheTime) {
+        //     $headerMenu = $cache->remember("headerMenu_" . app()->getLocale(), $cacheTime, function () {
+        //         return Menu::whereType("header")->order()->get();
+        //     });
+        //     $view->with(compact("headerMenu"));
+        // });
 
         // view()->composer("layout.topbar", function ($view) use ($cache, $cacheTime) {
         //     $languageList = $cache->remember("frontLanguageList", $cacheTime, function () {
