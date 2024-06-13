bf449806ed2cbcba172675200db344bb3320dbaa
Kuzey Oku Software <171201010@ogrenci.karatekin.edu.tr>
2024-06-13 16:25:50 +0300

    s

--- resources/views/layout/client.blade.php
+++ resources/views/layout/client.blade.php
@@ -1,13 +1,13 @@
 <section class="clients-section style-two">
     <div class="auto-container">
         <div class="sponsors-outer">
-            <ul class="clients-carousel owl-carousel owl-theme">
-                @foreach ($reference as $reference)
+            @foreach ($reference as $reference)
+                <ul class="clients-carousel owl-carousel owl-theme">
                     <li class="slide-item">
                         <a href="{{ $reference->url }}"><img src="{{ $reference->image_url }}" alt=""></a>
                     </li>
-                @endforeach
-            </ul>
+                </ul>
+            @endforeach
         </div>
     </div>
 </section>
