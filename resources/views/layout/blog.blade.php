3ed4a7d2b05cb5909f28914a24a8448dde3e8d23
Kuzey Oku Software <171201010@ogrenci.karatekin.edu.tr>
2024-06-14 16:15:06 +0300

    icon

--- resources/views/layout/blog.blade.php
+++ resources/views/layout/blog.blade.php
@@ -19,10 +19,10 @@
                         </div>
                         <div class="content-box border">
                             <span class="date">{{ $post->created_at->translatedFormat('d M, Y') }}</span>
-                            <span class="post-info"><i class="fa fa-user-circle"></i>{{ $post->user->name }}</span>
+                            <span class="post-info">@svg('fas-user-circle', 'icon-space'){{ $post->user->name }}</span>
                             <h5 class="title"><a href="{{ $post->url }}">{{ $post->title }}</a></h5>
                             <div class="text">{{ $post->short_description }}</div>
-                            <a href="{{ $post->url }}" class="read-more"><i class="fa fa-long-arrow-alt-right"></i>
+                            <a href="{{ $post->url }}" class="read-more"><i>@svg('fas-long-arrow-alt-right')</i>
                                 @lang('front/blog.txt3')</a>
                         </div>
                     </div>
