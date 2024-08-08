1af7c4a3da30bf227a135b6b901b325ea49d349e
Kuzey Oku Software <171201010@ogrenci.karatekin.edu.tr>
2024-08-08 15:33:38 +0300

    a

--- resources/views/layout/client.blade.php
+++ resources/views/layout/client.blade.php
@@ -4,7 +4,8 @@
             @foreach ($reference as $reference)
                 <ul class="clients-carousel owl-carousel owl-theme">
                     <li class="slide-item">
-                        <a href="{{ $reference->url }}"><img src="{{ $reference->image_url }}" alt=""></a>
+                        <a href="{{ $reference->url }}"><img src="{{ $reference->getFirstMediaUrl('cover') }}"
+                                alt="{{ $reference->title }}"></a>
                     </li>
                 </ul>
             @endforeach
