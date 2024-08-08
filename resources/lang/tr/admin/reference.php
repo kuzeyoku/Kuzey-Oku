6432287d1a68e419de329b4e7f5f85e078a22889
Kuzey Oku Software <171201010@ogrenci.karatekin.edu.tr>
2024-08-08 15:26:10 +0300

    rr

--- resources/lang/tr/admin/reference.php
+++ resources/lang/tr/admin/reference.php
@@ -9,6 +9,8 @@ return [
     "edit" => "Referans Düzenle",
     "edit_description" => "Referans Düzenle",
     "list" => "Referanslar",
+    "form_title" => "Referans Başlığı",
+    "form_title_placeholder" => "Referans Başlığı Giriniz",
     "form_url" => "Referans Bağlantısı",
     "form_url_placeholder" => "Referans Bağlantısı Giriniz",
     "form_image" => "Referans Görseli",
